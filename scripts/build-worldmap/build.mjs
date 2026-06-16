25791aa51927616a9743bf4954921a97a94d0199
Haokai Ding <hk_ding@outlook.com>
2026-06-16 23:24:21 +0800

    Optimize world-map SVG: merge land + round coords (162KB to 57KB)

--- scripts/build-worldmap/build.mjs
+++ scripts/build-worldmap/build.mjs
@@ -1,16 +1,16 @@
 import { writeFileSync } from 'node:fs';
 import { createRequire } from 'node:module';
 import { geoEquirectangular, geoPath } from 'd3-geo';
-import { feature } from 'topojson-client';
+import { merge } from 'topojson-client';
 
 const require = createRequire(import.meta.url);
 const world = require('world-atlas/countries-110m.json');
 
 const W = 1000, H = 500;
 const projection = geoEquirectangular().fitSize([W, H], { type: 'Sphere' });
-const path = geoPath(projection);
-const land = feature(world, world.objects.countries);
-const d = land.features.map(f => path(f)).filter(Boolean).join(' ');
+const path = geoPath(projection).digits(1);
+const land = merge(world, world.objects.countries.geometries);
+const d = path(land);
 
 const svg = `<svg id="vmWorld" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${W} ${H}" role="img" aria-label="World map of site visitors" preserveAspectRatio="xMidYMid meet">
 <path class="vm-land" d="${d}"/>
